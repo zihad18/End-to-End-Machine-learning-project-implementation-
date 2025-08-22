bf6ba25f68fbb5797d7d2947daf527e21553b45a
zihad18 <zihadmaruf397@gmail.com>
2025-08-22 09:51:29 +0600

    installation done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us-visa",
+    version="0.0.0",
+    author="Zihad",
+    author_email="zihadmaruf397@gmail.com",
+    packages=find_packages()
+)
