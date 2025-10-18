a12127f2f624014ceba566e35e42e9d7695518e7
zihad18 <zihadmaruf397@gmail.com>
2025-10-18 22:04:38 +0600

    constant added

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,14 @@
+import os
+from datetime import date
+
+DATABASE_NAME = "US_VISA"
+
+COLLECTION_NAME = "visa_data"
+
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "usvisa"
+
+ARTIFACTS_DIR: str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
