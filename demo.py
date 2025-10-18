0066c55f9c1e8c7bd3e140643513c87c6111ba2a
zihad18 <zihadmaruf397@gmail.com>
2025-10-18 22:25:40 +0600

    constant added

--- demo.py
+++ demo.py
@@ -1,25 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-from us_visa.utils import save_numpy_array_data
-import sys
-import numpy as np
-try:
-    logging.info("We are trying to save a numpy array")
-    array = np.array([1,2,3,4,5])
-    file_path = "array.npy"
-    save_numpy_array_data(file_path=file_path, array=array)
-    logging.info("We have saved the numpy array")
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys)
-    
+import os
 
-
-
-
-# try:
-#     r = 1/0
-#     print(r)
-# except Exception as e:
-#     logging.info(e)
-#     raise USvisaException(e, sys)
+mongodburl = os.getenv("MONGODB_URL")
+print(f"MongoDB URL: {mongodburl}")
--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -12,3 +12,22 @@ PIPELINE_NAME: str = "usvisa"
 ARTIFACTS_DIR: str = "artifact"
 
 MODEL_FILE_NAME = "model.pkl"
+
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+FILE_NAME: str = "usvisa.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
